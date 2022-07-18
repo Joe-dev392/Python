ff54e6f10e45e9b0a79bb7fedcd07e2fb8117831
Joe-dev392 <fatizee01@gmail.com>
2022-07-18 14:00:46 -0700

    learning python

--- console-apps/apps/app4/main.py
+++ console-apps/apps/app4/main.py
@@ -2,8 +2,32 @@ class Library:
     def __init__(self, booklist):
         self.books = booklist
 
+    def displayAvailableBooks(self):
+        print(f"There are many {len(self.books)} books available")
+        for book in self.books:
+            print(f"==> {book}")
+
+    def checkOutBooks(self, name, bookName):
+        pass
+
+    def checkInBooks(self, bookName):
+        pass
+
+    def populateBookList(self, bookName):
+        pass
+
+
+
+
 class Person():
-    pass
+    def issueBook(self):
+        pass
+
+    def returnBook(self):
+        pass
+
+    def addBook(self):
+        pass
 
 if __name__ == '__main__':
     wuseLibrary = Library(['General Maths', 'Programming Fundemantals', 'Ikemefuna'])
@@ -12,4 +36,25 @@ if __name__ == '__main__':
     print("<======== Wuse Digital Libraary ========>")
     print("_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-")
     print("\nWelcome user, what would you like to accomplish today?")
-    print("\n1. View Book List \n2. ")
+    print("\n1. View Book List \n2. Check out a book \n3. Add a new book \4. Check in a book \n5. Track book(s) \n6. Quit")
+
+    while True:
+        try:
+            action = int(input("Kindly enter an option\t"))
+            if action == 1:
+                wuseLibrary.displayAvailableBooks()
+            elif action == 2:
+                pass
+            elif action == 3:
+                pass
+            elif action == 4:
+                pass
+            elif action == 5:
+                pass
+            elif action == 6:
+                print("Thanks for showing me love")
+                exit()
+
+        except Exception as anomaly:
+            print(anomaly)
+    
