730f6f4f0ef3250133da724f3ac769d729534298
Joe-dev392 <fatizee01@gmail.com>
2022-07-18 15:18:18 -0700

    Python library system

--- console-apps/apps/app4/main.py
+++ console-apps/apps/app4/main.py
@@ -8,35 +8,52 @@ class Library:
             print(f"==> {book}")
 
     def checkOutBooks(self, name, bookName):
-        pass
+        if bookName not in self.books:
+            print(f"{bookName} is unavailable in our library")
+        else:
+            self.books.remove(bookName)
+            trackList.append({name:bookName})
+            print(f"{bookName} checked out successfully")
 
     def checkInBooks(self, bookName):
-        pass
+        self.books.append(bookName)
+        print(f"Book {bookName} checked in successfully")
 
     def populateBookList(self, bookName):
-        pass
+        self.books.append(bookName)
+        print(f"Book \"{bookName}\" added successfully")
 
 
 
 
 class Person():
     def issueBook(self):
-        pass
+        print("You wanna issue out a book")
+        self.books = input("Please enter the book name ")
+        return self.books
 
     def returnBook(self):
-        pass
+        print("You wanna return a book?")
+        self.books = input("Enter book name ")
+        borrower = input("Enter the borrower name ")
+        if {borrower: self.books} in trackList:
+            trackList.remove({borrower: self.books})
+        return self.books
 
     def addBook(self):
-        pass
+        print("A donated new book")
+        self.books = input("Please enter book name ")
+        return self.books
 
 if __name__ == '__main__':
     wuseLibrary = Library(['General Maths', 'Programming Fundemantals', 'Ikemefuna'])
     individual = Person()
+    trackList = []
 
     print("<======== Wuse Digital Libraary ========>")
     print("_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-")
     print("\nWelcome user, what would you like to accomplish today?")
-    print("\n1. View Book List \n2. Check out a book \n3. Add a new book \4. Check in a book \n5. Track book(s) \n6. Quit")
+    print("\n1. View Book List \n2. Check out a book \n3. Add a new book \n4. Check in a book \n5. Track book(s) \n6. Quit")
 
     while True:
         try:
@@ -44,17 +61,22 @@ if __name__ == '__main__':
             if action == 1:
                 wuseLibrary.displayAvailableBooks()
             elif action == 2:
-                pass
+                wuseLibrary.checkOutBooks(input("Please provide the borrower's name\t "), individual.issueBook())
             elif action == 3:
-                pass
+                wuseLibrary.populateBookList(individual.addBook())
             elif action == 4:
-                pass
+                wuseLibrary.checkInBooks(individual.returnBook())
             elif action == 5:
-                pass
+                for item in trackList:
+                    for key, value in item.items():
+                        borrower = key
+                        book = value
+                        print(f"==> Book: {book} | Borrower: {borrower}")
+                if len(trackList) == 0:
+                    print("No book has been borrowed")
             elif action == 6:
                 print("Thanks for showing me love")
                 exit()
 
         except Exception as anomaly:
             print(anomaly)
-    
